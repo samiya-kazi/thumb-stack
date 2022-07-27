07a92164674419727f34b0c436ce9cd16ea50c4a
Samiya Kazi <samiya.kazi09@gmail.com>
2022-07-27 22:41:55 +0600

    added transformer to square element

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -8,21 +8,57 @@ import Toolbar from './Toolbar';
 function Editor () {
 
   const [shapes, setShapes] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      setSelectedId(null);
+    }
+  };
 
   return (
     <div className='editor'>
       <Toolbar setShapes={setShapes}/>
       <div className='drawing-stage'>
-        <Stage width={640} height={350}>
+        <Stage width={640} height={350} onMouseDown={checkDeselect} onTouchStart={checkDeselect}>
           <Layer>
             {shapes.map((shape) => {
               switch (shape.type) {
                 case 'square':
-                  return <SquareElement shape={shape} setShapes={setShapes} key={shape.key} />
+                  return <SquareElement 
+                    shape={shape} 
+                    setShapes={setShapes} 
+                    key={shape.key} 
+                    isSelected={shape.id === selectedId}
+                    onSelect={() => {
+                      setSelectedId(shape.id);
+                    }}
+                    />
+
                 case 'circle':
-                  return <CircleElement shape={shape} setShapes={setShapes} key={shape.key} />
+                  return <CircleElement 
+                    shape={shape} 
+                    setShapes={setShapes} 
+                    key={shape.key} 
+                    isSelected={shape.id === selectedId}
+                    onSelect={() => {
+                      setSelectedId(shape.id);
+                    }}
+                    />
+
                 case 'star':
-                  return <StarElement shape={shape} setShapes={setShapes} key={shape.key} />
+                  return <StarElement 
+                    shape={shape} 
+                    setShapes={setShapes} 
+                    key={shape.key} 
+                    isSelected={shape.id === selectedId}
+                    onSelect={() => {
+                      setSelectedId(shape.id);
+                    }}
+                    />
+
                 default: return null;
               }
             })}
--- client/src/Components/SquareElement.js
+++ client/src/Components/SquareElement.js
@@ -1,6 +1,18 @@
-import { Rect } from "react-konva"
+import { useRef, useEffect } from "react";
+import { Rect, Transformer } from "react-konva"
 
-function SquareElement ({ shape, setShapes }) {
+function SquareElement ({ shape, setShapes, isSelected, onSelect }) {
+
+  const shapeRef = useRef();
+  const trRef = useRef();
+
+  useEffect(() => {
+    if (isSelected) {
+      //attaches transformer
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
 
   function handleDragEnd (event) {
 
@@ -16,19 +28,62 @@ function SquareElement ({ shape, setShapes }) {
     })
   }
 
+
+  function setTransformation () {
+    // transformer is changing scale of the node but we want the new width and height to store.
+    const node = shapeRef.current;
+    const scaleX = node.scaleX();
+    const scaleY = node.scaleY();
+
+    // we will reset it back
+    node.scaleX(1);
+    node.scaleY(1);
+
+    const x = node.x();
+    const y = node.y();
+    const width = Math.max(5, node.width() * scaleX);
+    const height= Math.max(node.height() * scaleY);
+
+    setShapes((prevlist) => {
+      const newlist = [...prevlist].filter((listshape) => listshape.id !== shape.id);
+      newlist.push({
+        ...shape,
+        x,
+        y,
+        width,
+        height
+      })
+
+      return newlist;
+    })
+
+  }
+
   return (
     <>
     <Rect
-      id={shape.id}
-      key={shape.key}
-      x={shape.x}
-      y={shape.y}
-      width={shape.width}
-      height={shape.height}
-      fill={shape.fill}
-      draggable={true}
+      {...shape}
       onDragEnd = {handleDragEnd}
+      onClick={onSelect}
+      onTap={onSelect}
+      ref={shapeRef}
+      onTransformEnd={setTransformation}
     />
+
+    {isSelected && (
+      <Transformer
+        ref={trRef}
+        boundBoxFunc={(oldBox, newBox) => {
+
+          // limit resize
+          if (newBox.width < 10 || newBox.height < 10) {
+            return oldBox;
+          }
+
+          return newBox;
+        }}
+      />
+    )}
 
     </>
   )
