4e205d005f9ea8ec806fbe1f616438da19fbc35c
Samiya Kazi <samiya.kazi09@gmail.com>
2022-07-29 02:16:51 +0600

    added simple styling

--- client/src/Components/SavedThumbnails.js
+++ client/src/Components/SavedThumbnails.js
@@ -4,11 +4,13 @@ function SavedThumbnails ({ thumbnails, setSelectedThumbnail }) {
   return (
     <div className="thumbnail-container">
       <h3>Saved Thumbnails</h3>
+
+      <button key='new' onClick={() => {setSelectedThumbnail(null)}}>New</button>
+
       {thumbnails.map((thumbnail, key) => {
-        return <button key={key} onClick={() => {setSelectedThumbnail(thumbnail)}}>{key}</button>
+        return <button key={key} onClick={() => {setSelectedThumbnail(thumbnail)}}>{key + 1}</button>
       })}
 
-    <button key='new' onClick={() => {setSelectedThumbnail(null)}}>New</button>
 
     </div>
   )
